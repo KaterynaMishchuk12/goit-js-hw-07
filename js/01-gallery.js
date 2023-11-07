d8c49e175ff4f9fdcdcbcd64a0259f8465408032
KaterynaMishchuk12 <smile.sonrisa7@gmail.com>
2023-11-07 16:42:16 +0200

    first task done

---
+++ js/01-gallery.js
@@ -0,0 +1,55 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const galleryMarkup = createGalleryItemsMarkup(galleryItems);
+
+galleryEl.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryEl.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  const currentItemSrc = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `<img width ="1280" height="auto" src="${currentItemSrc}">`,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+  instance.show();
+
+  function onEscKeyPress(event) {
+    const ESC_KEY_CODE = "Escape";
+    if (event.code === ESC_KEY_CODE) {
+      instance.close();
+    }
+  }
+}
+
+function createGalleryItemsMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+<a class="gallery__link" href="${original}g">
+   <img
+      class="gallery__image"
+      src="${preview}"
+       data-source="${original}"
+      alt="${description}"
+     />
+   </a>
+ </li>`;
+    })
+    .join(" ");
+}
